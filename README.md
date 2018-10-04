ccb2eefbb32ac0b0c16ccbd0caccda8e9231b0e8
Chao wen <chaowen.nthu@gmail.com>
2018-10-04 22:48:12 +0800

    clean testbed

--- README.md
+++
@@ -1,5 +0,0 @@
-# Scalable Video Streaming with Media Aware-Network Element implement in P4 Language
-
----
-
-**Make sure that you edit env.sh to point to your local copy of bmv2 and p4c-bm.**
--- run_demo.sh
+++ run_demo.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Copyright 2013-present Barefoot Networks, Inc. 
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source env.sh
@@ -28,7 +14,7 @@ CLI_PATH=$BMV2_PATH/tools/runtime_CLI.py
 #$P4C_BM_SCRIPT p4src/source_routing.p4 --json source_routing.json
 p4c-bm2-ss --arch v1model -o demo.json \
 		--p4runtime-file mytunnel.p4info --p4runtime-format text \
-		p4src/demo.p4
+		p4src/task1.p4
 
 # This gives libtool the opportunity to "warm-up"
 sudo $SWITCH_PATH >/dev/null 2>&1
